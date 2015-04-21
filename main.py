c3e1ebb4cb9babdf0aaf95a23337944e5e8dfdec
tt_tsai <tt_tsai@htc.com>
2015-04-21 13:43:49 +0800

    init commit

---
+++ main.py
@@ -0,0 +1,149 @@
+﻿#!/usr/bin/env python3
+
+import tkinter as tk
+from os import listdir
+from os.path import isdir, isfile, exists
+
+class SettingBoard(tk.LabelFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        #data member
+        self.sup_type = [".jpg"]
+        self.bg_color = '#D0D0D0'
+        self.load_path = tk.StringVar()
+        self.load_path.set("C:\\Users\\TT_Tsai\\Dropbox\\Photos\\Sample Album")  #for debug
+        self.pic_filenames = []
+        self.row_fname = 0
+        self.cb_pic_added = []
+
+        #board
+        self.config(text='settings', bg=self.bg_color)
+
+        #load pic files
+        row = 0
+        tk.Button(self, text="Load Pic...", command=self.doLoadPic).grid(row=row, column=0, sticky='w')
+        tk.Entry(self, textvariable=self.load_path).grid(row=row, column=1, columnspan = 99, sticky='we')
+        row += 1
+        self.row_fname = row
+
+        #load gpx file
+        row += 1
+        tk.Button(self, text="Load Gpx...").grid(row=row, sticky='w')
+        row += 1
+        tk.Label(self, text="(no gpx)", bg=self.bg_color).grid(row=row, sticky='w')
+
+    def isSupportType(self, fname):
+        fname = fname.lower()
+
+        for t in self.sup_type:
+            if(fname.endswith(t)):
+                return True
+        return False
+
+    #add pic filename if cond is ok
+    def addPicFile(self, f):
+        if(not self.isSupportType(f)):
+            return False
+        if(f in self.pic_filenames):
+            return False
+
+        self.pic_filenames.append(f)
+        return True
+
+    #load pic filenames from dir/file
+    def doLoadPic(self):
+        path = self.load_path.get()
+
+        #error check
+        if(path is None or len(path) == 0):
+            return
+        if(not exists(path)):
+            print("The path does not exist")
+            return
+
+        #add file, if support
+        has_new = False
+        if(isdir(path)):
+            for f in listdir(path):
+                if(self.addPicFile(f)):
+                    has_new = True
+        elif(isfile(path)):
+            if(self.addPicFile(f)):
+                has_new = True
+        else:
+            print("Unknown type: " + path)  #show errmsg
+            return
+
+        if(has_new):
+            self.dispPicFilename()
+            self.doPicAdded()
+
+    #disp pic filename 
+    def dispPicFilename(self):
+        fname_txt = ""
+        for f in self.pic_filenames:
+            fname_txt += f
+            fname_txt += " "
+        #self.label_fname["text"] = fname_txt
+        label_fname = tk.Label(self)
+        label_fname.config(text=fname_txt, bg=self.bg_color)
+        label_fname.grid(row=self.row_fname, column=0, columnspan=99, sticky='w')
+
+        #for f in self.pic_filenames:
+            #tk.Button(self, text=f, relief='groove').grid(row=self.row_fname, column=self.pic_filenames.index(f))
+
+    def getPicFilenames(self):
+        return self.pic_filenames
+
+    def onPicAdded(self, cb):
+        self.cb_pic_added.append(cb)
+
+    def doPicAdded(self):
+        for cb in self.cb_pic_added:
+            cb(self.pic_filenames)
+            
+class PicBoard(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master, bg='#000000')
+
+        #data member
+        self.pic_filenames = []
+
+    def addPic(self, fnames):
+        self.pic_filenames.extend(fnames)
+        #reset pic block
+        for f in self.pic_filenames:
+            tk.Button(self, text=f).grid(row=self.pic_filenames.index(f), sticky='news')
+
+class DispBoard(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        #data member
+        self.bg_color='#808080'
+
+        #board
+        self.config(bg=self.bg_color)
+
+        tk.Label(self, text="Dispaly Pic/Map", font='monaco 24', bg=self.bg_color).pack(expand=1, fill='both')
+
+
+root = tk.Tk()
+root.title("PicGisEditor")
+root.geometry('400x300')
+
+pad_ = 2
+setting_board = SettingBoard(root)
+setting_board.pack(side='top', anchor='nw', expand=0, fill='x', padx=pad_, pady=pad_)
+
+pic_board = PicBoard(root)
+pic_board.pack(side='left', anchor='nw', expand=0, fill='y', padx=pad_, pady=pad_)
+#add callback on pic added
+setting_board.onPicAdded(lambda fname:pic_board.addPic(fname))
+
+disp_board = DispBoard(root)
+disp_board.pack(side='right', anchor='se', expand=1, fill='both', padx=pad_, pady=pad_)
+
+
+root.mainloop()
