868ebd1d71d167e07b27b9be0a8a3c73d0f41c8d
tt_tsai <tt_tsai@htc.com>
2015-06-08 12:53:57 +0800

    1. create tile module

--- main.py
+++ main.py
@@ -7,82 +7,10 @@ import urllib.request
 import shutil
 from os import listdir
 from os.path import isdir, isfile, exists
-from PIL import Image, ImageTk
-
-class TileSystem:
-    EARTH_RADIUS = 6378137
-    MIN_LATITUDE = -85.05112878
-    MAX_LATITUDE = 85.05112878
-    MIN_LONGITUDE = -180
-    MAX_LONGITUDE = 180
-
-    @staticmethod
-    def crop(val, min_val, max_val):
-        return min(max(val, min_val), max_val)
-
-    @staticmethod
-    def getMapSize(level):
-        return 256 << level
-
-    @classmethod
-    def getValidLatitude(C, latitude):
-        return C.crop(latitude, C.MIN_LATITUDE, C.MAX_LATITUDE)
-
-    @classmethod
-    def getValidLongitude(C, latitude):
-        return C.crop(latitude, C.MIN_LONGITUDE, C.MAX_LONGITUDE)
-
-    @classmethod
-    def getGroundResolution(C, latitude, level):
-        return math.cos(latitude * math.pi / 180) * 2 * math.pi * C.EARTH_RADIUS / C.getMapSize(level)
-
-    @classmethod
-    def getMapScale(C, latitude, level, screen_dpi):
-        return C.getGroundResolution(latitude, level) * screen_dpi / 0.0254
-
-    @classmethod
-    def getPixcelXYByLatLon(C, latitude, longitude, level):
-        latitude = C.crop(latitude, C.MIN_LATITUDE, C.MAX_LATITUDE)
-        longitude = C.crop(longitude, C.MIN_LONGITUDE, C.MAX_LONGITUDE)
-
-        x = (longitude + 180) / 360 
-        sin_latitude = math.sin(latitude * math.pi / 180)
-        y = 0.5 - math.log((1 + sin_latitude) / (1 - sin_latitude)) / (4 * math.pi)
-
-        map_size = C.getMapSize(level)
-        pixel_x = int(C.crop(x * map_size + 0.5, 0, map_size - 1))
-        pixel_y = int(C.crop(y * map_size + 0.5, 0, map_size - 1))
-
-        return (pixel_x, pixel_y)
-    
-
-    @classmethod
-    def getLatLonByPixcelXY(C, pixel_x, pixel_y, level):
-        map_size = C.getMapSize(level)
-        x = (C.crop(pixel_x , 0, map_size - 1) / map_size) - 0.5
-        y = 0.5 - (C.crop(pixel_y , 0, map_size - 1) / map_size)
-
-        lat = 90 - 360 * math.atan(math.exp(-y * 2 * math.pi)) / math.pi
-        lon = x * 360
-        return (lat, lon)
-
-    @staticmethod
-    def getTileXYByPixcelXY(pixel_x, pixel_y):
-        tile_x = int(pixel_x / 256)
-        tile_y = int(pixel_y / 256)
-        return (tile_x, tile_y)
-
-    @staticmethod
-    def getPixcelXYByTileXY(tile_x, tile_y):
-        pixel_x = tile_x * 256
-        pixel_y = tile_y * 256
-        return (pixel_x, pixel_y)
-
-    @classmethod
-    def getTileXYByLatLon(C, latitude, longitude, level):
-        (px, py) = C.getPixcelXYByLatLon(latitude, longitude, level)
-        return C.getTileXYByPixcelXY(px, py)
-
+from PIL import Image, ImageTk, ImageDraw
+#my modules
+import tile
+from tile import  TileSystem, TileMap
 
 class SettingBoard(tk.LabelFrame):
     def __init__(self, master):
@@ -273,80 +201,6 @@ class DispBoard(tk.Frame):
         self.disp_label.config(image=photo)
         self.disp_label.image = photo #keep a ref
 
-class TileMap:
-    def getMapName(self): return self.map_title
-
-    def __init__(self):
-        #self.map_id = "TM25K_2001"
-        #self.map_title = "2001-臺灣經建3版地形圖-1:25,000"
-        #self.lower_corner = (117.84953432, 21.65607265)
-        #self.upper_corner = (123.85924109, 25.64233621)
-        #self.url_template = "http://gis.sinica.edu.tw/tileserver/file-exists.php?img=TM25K_2001-jpg-%d-%d-%d"
-        #self.level_min = 7
-        #self.level_max = 16
-
-        self.__cache_dir = './cache'
-        if not os.path.exists(self.__cache_dir):
-            os.makedirs(self.__cache_dir)
-
-        self.__img_repo = {}
-
-    def isSupportedLevel(self, level):
-        return self.level_min <= level and level <= self.level_max
-
-    def genTileName(self, level, x, y):
-        return "%s-%d-%d-%d.jpg" % (self.map_id, level, x, y)
-
-    #return tkinter.PhotoImage
-    def getTileByLonLat(self, level, longitude, latitude):
-        (x, y) = TileSystem.getTileXYByLatLon(latitude, longitude, level)
-        return self.getTileByTileXY(level, x, y)
-
-    def getTileByTileXY(self, level, x, y):
-        name = self.genTileName(level, x, y)
-
-        img = self.__img_repo.get(name)
-        if img is None:
-            img = self.__readTile(level, x, y)
-            self.__img_repo[name] = img
-        return img
-
-    def __readTile(self, level, x, y):
-        path = "%s/%s" % (self.__cache_dir, self.genTileName(level, x, y))
-        if not os.path.exists(path):
-            self.__downloadTile(level, x, y, path)
-        print(path)
-        return Image.open(path)
-
-    def __downloadTile(self, level, x, y, file_path):
-        url = self.url_template % (level, x, y)
-
-        #urllib.request.urlretrieve(url, file_path)
-        with urllib.request.urlopen(url) as response, open(file_path, 'wb') as out_file:
-            shutil.copyfileobj(response, out_file)
-        print(url)
-
-def getTM25Kv3TileMap():
-    tm = TileMap()
-    tm.map_id = "TM25K_2001"
-    tm.map_title = "2001-臺灣經建3版地形圖-1:25,000"
-    tm.lower_corner = (117.84953432, 21.65607265)
-    tm.upper_corner = (123.85924109, 25.64233621)
-    tm.url_template = "http://gis.sinica.edu.tw/tileserver/file-exists.php?img=TM25K_2001-jpg-%d-%d-%d"
-    tm.level_min = 7
-    tm.level_max = 16
-    return tm
-
-def getTM25Kv4TileMap():
-    tm = TileMap()
-    tm.map_id = "TM25K_2003"
-    tm.map_title = "2001-臺灣經建4版地形圖-1:25,000"
-    tm.lower_corner = (117.84953432, 21.65607265)
-    tm.upper_corner = (123.85924109, 25.64233621)
-    tm.url_template = "http://gis.sinica.edu.tw/tileserver/file-exists.php?img=TM25K_2003-jpg-%d-%d-%d"
-    tm.level_min = 5
-    tm.level_max = 17
-    return tm
 
 class MapController:
     def getTileMap(self): return self.tile_map
@@ -389,7 +243,7 @@ class MapController:
 
     def __init__(self):
         self.level = 16
-        self.tile_map = getTM25Kv3TileMap()
+        self.tile_map = tile.getTM25Kv3TileMap()
         #self.center_lon = 121.334754
         #self.center_lat = 24.987969
         self.setLonLat(lon=121.334754, lat=24.987969)
@@ -401,27 +255,40 @@ class MapController:
         self.geo_py += int(py)
 
     def getTileImage(self, width, height):
+        img = self.__getTileImage(width, height)
 
+        #draw center cross
+        cross_x = width/2
+        cross_y = height/2
+        cross_len = 10
+        cross_color = '#808080'
+        draw = ImageDraw.Draw(img)
+        draw.line((cross_x-cross_len, cross_y, cross_x+cross_len, cross_y), fill=cross_color, width=2)
+        draw.line((cross_x, cross_y-cross_len, cross_x, cross_y+cross_len), fill=cross_color, width=2)
+        del draw
+
+        return img
+
+    def __getTileImage(self, width, height):
         #parameters
         left = self.geo_px
         up = self.geo_py
         right = self.geo_px + width
         low = self.geo_py + height
 
-        #get from buffered image:
+        #get from buffered image =====================
         if self.buff_img_attr is not None:
             (img_level, img_left, img_up, img_right, img_low) = self.buff_img_attr
 
             #new image is within buff_img
             if img_level == self.level and img_left <= left and img_up <= up and img_right >= right and img_low >= low:
-                #print("get from buffered image")
-
                 #crop
                 img_x = left - img_left
                 img_y = up - img_up
+                #print("get from buffered image")
                 return self.buff_img.crop((img_x, img_y, img_x + width, img_y + height))
 
-        #print("gen new image")
+        #gen new image =========================
         #get tile x, y.
         (t_left, t_upper) = TileSystem.getTileXYByPixcelXY(left, up)
         (t_right, t_lower) = TileSystem.getTileXYByPixcelXY(right, low)
@@ -442,8 +309,8 @@ class MapController:
         #crop
         img_x = self.geo_px - (t_left*256)
         img_y = self.geo_py - (t_upper*256)
+        #print("gen new image")
         return img.crop((img_x, img_y, img_x + width, img_y + height))
-
 
 if __name__ == '__main__':
 
