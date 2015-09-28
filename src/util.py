ab973e5fb612f725ab107db1fab817a5edd6aee9
dayanuyim <dayanuyim@gmail.com>
2015-09-28 21:15:51 +0800

    ceate UI of buttons of area selector

--- src/util.py
+++ src/util.py
@@ -2,6 +2,8 @@
 
 import tkinter as tk
 from PIL import Image, ImageTk, ImageDraw
+#my modules
+import conf
 
 class AreaSelector:
     @property
@@ -39,13 +41,16 @@ class AreaSelector:
         self.__cv_cancel = self.genCancelButton()
         self.__cv_items.append(self.__cv_cancel)
 
+        self.__cv_setting = self.genSettingButton()
+        self.__cv_items.append(self.__cv_setting)
+
         #bind
         for item in self.__cv_items:
             canvas.tag_bind(item, "<Button-1>", self.onSelectAreaClick)
             canvas.tag_bind(item, "<Button1-ButtonRelease>", self.onSelectAreaRelease)
             canvas.tag_bind(item, "<Button1-Motion>", self.onSelectAreaMotion)
 
-
+    #{{ events
     #bind motion events
     def onSelectAreaClick(self, e):
         self.__mousepos = (e.x, e.y)
@@ -62,20 +67,34 @@ class AreaSelector:
         #update
         self.__mousepos = (e.x, e.y)
 
-    def genOKButton(self, order=1):
-        #anchor ne
-        w = self.__pos[0] + self.__size[0] - self.__button_side*order
-        h = self.__pos[1]
-        return self.__canvas.create_oval(w, h, w+self.__button_side, h+self.__button_side, fill='green')
+    #}}
 
-    def genCancelButton(self, order=2):
-        #anchor ne
-        w = self.__pos[0] + self.__size[0] - self.__button_side*order
-        h = self.__pos[1]
-        return self.__canvas.create_oval(w, h, w+self.__button_side, h+self.__button_side, fill='red')
-
+    #{{ canvas items
     def genAreaImage(self):
         img = Image.new('RGBA', self.__size, (255,255,0, 128))  #yellow 
         pimg = ImageTk.PhotoImage(img)
         self.__cv_area_img = pimg   #keep refg
         return self.__canvas.create_image(self.__pos, image=pimg, anchor='nw')
+
+    def genOKButton(self, order=1):
+        x = self.__pos[0] + self.__size[0] - self.__button_side*order #x of upper-left
+        y = self.__pos[1]  #y of upper-left
+        return self.__canvas.create_oval(x, y, x+self.__button_side, y+self.__button_side, fill='green')
+
+    def genCancelButton(self, order=2):
+        n = int(self.__button_side/4)
+        x = self.__pos[0] + self.__size[0] - self.__button_side*order + 2*n  #x of center
+        y = self.__pos[1] + 2*n #y of center
+        cross = ((0,n), (n,2*n), (2*n,n), (n,0), (2*n,-n), (n,-2*n), (0,-n), (-n,-2*n), (-2*n,-n), (-n,0), (-2*n,n), (-n, 2*n))
+        cancel_cross = []
+        for pt in cross:
+            cancel_cross.append((pt[0]+x, pt[1]+y))
+        return self.__canvas.create_polygon(cancel_cross, fill='red')
+
+    def genSettingButton(self, order=3):
+        n = int(self.__button_side/2)
+        x = self.__pos[0] + self.__size[0] - self.__button_side*order + n  #x of center
+        y = self.__pos[1] + n #y of center
+        return self.__canvas.create_text(x, y, font='Arialuni 16 bold', text='S', fill='#404040')
+
+    #}}
