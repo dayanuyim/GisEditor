fc229ca3f802f9a1796292bbcadd5bcec27e8cb8
dayanuyim <dayanuyim@gmail.com>
2018-01-24 17:08:11 +0800

    Fix: Saving panel is not transparent on MAC
    - workaround to draw transprent partially
      because PIL only support full or no transprent on MAC, i.g. only 0/255 alpha value accepted.

--- src/util.py
+++ src/util.py
@@ -103,6 +103,48 @@ def drawTextBg(draw, xy, text, fill, font, bg_fill="white"):
     #text
     draw.text(xy, text, fill=fill, font=font)
 
+
+def flattenAlpha(img):
+    alpha = img.split()[-1]  # Pull off the alpha layer
+    ab = alpha.tobytes()  # Original 8-bit alpha
+
+    checked = []  # Create a new array to store the cleaned up alpha layer bytes
+
+    # Walk through all pixels and set them either to 0 for transparent or 255 for opaque fancy pants
+    transparent = 50  # change to suit your tolerance for what is and is not transparent
+
+    p = 0
+    for pixel in range(0, len(ab)):
+        if p % 2:
+            checked.append(0)  # Transparent
+        else:
+            checked.append(255)  # Opaque
+        p += 1
+
+    mask = Image.frombytes('L', img.size, bytes(checked))
+    img.putalpha(mask)
+    return img
+
+# PIL only support 0/255 alpha value on MAC, ref:
+#   https://stackoverflow.com/questions/41576637/are-rgba-pngs-unsupported-in-python-3-5-pillow
+def macAlpha(img):
+
+    w, h = img.size
+
+    transp = (0,) * w
+    opaque = (255,) * w
+
+    alpha = []
+    for i in range(h):
+        if i % 2:
+            alpha.extend(opaque)
+        else:
+            alpha.extend(transp)
+
+    mask = Image.frombytes('L', img.size, bytes(alpha))
+    img.putalpha(mask)
+    return img
+
 # Notice: 'accelerator string' may not a perfect guess, need more heuristic improvement
 def __guessAccelerator(event):
     return event.strip('<>').replace('Control', 'Ctrl').replace('-', '+')
@@ -582,13 +624,20 @@ class AreaSelector:
         self.__canvas.delete(name)
         self.__genPanel(name, pos, size)
 
+
     def __genPanel(self, name, pos, size):
         #area img
-        r, g, b = ImageColor.getrgb(self.__panel_color)
         w = max(1, size[0])
         h = max(1, size[1])
-        img = Image.new('RGBA', (w,h), (r, g, b, 96))  #transparent
+
+        if platform.system() == "Darwin":
+            img = Image.new('RGBA', (w,h), self.__panel_color)
+            img = macAlpha(img)  # experimental: workaround for drawing alpha on MAC
+        else:
+            color = ImageColor.getrgb(self.__panel_color) + (96,) #transparent
+            img = Image.new('RGBA', (w,h), color)
         img = ImageTk.PhotoImage(img) #to photo image
+
         #area item
         item = self.__canvas.create_image(pos, image=img, anchor='nw', tag=('AS',name))
         #bind
